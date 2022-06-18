3b20e4386c69550444b69015febc39ad3f8f3ba1
Vinicius Lopes Simoes <vini.lopes.simoes@gmail.com>
2022-06-18 20:35:42 -0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		// Insert your components inside the App
+		<div className="App">
+			<h1>Minimal React App</h1>
+		</div>
+	);
+}
+
+export default App;
